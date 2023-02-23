a9214b2f0b72ebfefaa78abad96eac315f1ebcfc
MustafaAldelaimi <mustafa1007@yahoo.com>
2023-02-23 11:01:21 +0000

    Bonus complete

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -14,8 +14,8 @@ router.get("/", async (req, res) => {
           const data = await Restaurant.findAll();
           res.json(data)
 })
-
-router.post("/", [check("name").not().isEmpty().trim(), check("location").not().isEmpty().trim(), check("cuisine").not().isEmpty().trim()], async (req, res) => {
+,
+router.post("/", [check("name").not().isEmpty().trim(), check("location").not().isEmpty().trim(), check("cuisine").not().isEmpty().trim(), check("name").isLength({min:10, max:30})], async (req, res) => {
           const errors = validationResult(req);
           if (!errors.isEmpty()) {
                     return res.json({errors: errors.array()})
